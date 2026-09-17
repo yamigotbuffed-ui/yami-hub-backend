325fb81d6a3fc31add0af0a09f53ba4ad0f18197
yamigotbuffed-ui <you@example.com>
2026-09-17 12:33:42 +0100

    fix: create data folder on startup

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const bcrypt = require("bcryptjs");
+const fs = require("fs");
 const path = require("path");
 
-const adapter = new FileSync(path.join(__dirname, "data", "db.json"));
+const dataDir = path.join(__dirname, "data");
+if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
+
+const adapter = new FileSync(path.join(dataDir, "db.json"));
 const db = low(adapter);
 
 const defaultPasswordHash = bcrypt.hashSync("yamihub", 10);
